85608a40ffa47a449f17cbcc56513cce809110cb
Simon Chervenak <sarras305@gmail.com>
2020-02-02 18:02:00 -0500

    box

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ class Game:
             screen.refresh()
 
     def display(self, tile, x, y):
-        self.screen.print_at(f"{tile.h}", x + 3, y, colour=169)
-        self.screen.print_at(f"{tile.f}", x, y + 1, colour=28)
-        self.screen.print_at(f"{tile.i}", x + 6, y + 1, colour=136)
-        self.screen.print_at(f"{tile.r}", x + 3, y + 2)
-        self.screen.print_at(f"{tile.g}", x, y + 3, colour=226)
-        self.screen.print_at(f"{tile.s}", x + 6, y + 3, colour=21)
+        self.screen.print_at("┌─────────┐", x, y)
+        for i in range(1, 5):
+            self.screen.print_at("│", x, y + i)
+        for i in range(1, 5):
+            self.screen.print_at("│", x + 10, y + i)
+        self.screen.print_at("└─────────┘", x, y + 5)
+        self.screen.print_at(f"{tile.h}", x + 5, y + 1, colour=169)
+        self.screen.print_at(f"{tile.f}", x + 2, y + 2, colour=28)
+        self.screen.print_at(f"{tile.i}", x + 8, y + 2, colour=136)
+        self.screen.print_at(f"{tile.r}", x + 5, y + 3)
+        self.screen.print_at(f"{tile.g}", x + 2, y + 4, colour=226)
+        self.screen.print_at(f"{tile.s}", x + 8, y + 4, colour=21)
 
 
 Screen.wrapper(Game)
