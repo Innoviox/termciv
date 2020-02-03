6be251de854a8f1e5d1bfda4c750292571acf62a
Simon Chervenak <sarras305@gmail.com>
2020-02-03 13:09:09 -0500

    magic

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from asciimatics.screen import Screen
 from civ import Civilization
 from map import Map
 
+terrain_colors = {
+    'desert': 226,
+    'forest': 22,
+    'ocean': 19,
+    'plains': 118,
+    'mountain': 255
+}
+
 class Game:
     def __init__(self, screen):
         self.screen = screen
@@ -15,18 +23,19 @@ class Game:
             screen.refresh()
 
     def display(self, tile, x, y):
-        self.screen.print_at("┌─────────┐", x, y)
-        for i in range(1, 5):
-            self.screen.print_at("│", x, y + i)
-        for i in range(1, 5):
-            self.screen.print_at("│", x + 10, y + i)
-        self.screen.print_at("└─────────┘", x, y + 5)
-        self.screen.print_at(f"{tile.h}", x + 5, y + 1, colour=169)
-        self.screen.print_at(f"{tile.f}", x + 2, y + 2, colour=28)
-        self.screen.print_at(f"{tile.i}", x + 8, y + 2, colour=136)
-        self.screen.print_at(f"{tile.r}", x + 5, y + 3)
-        self.screen.print_at(f"{tile.g}", x + 2, y + 4, colour=226)
-        self.screen.print_at(f"{tile.s}", x + 8, y + 4, colour=21)
+        self.screen.print_at(" ", x, y, bg=terrain_colors[tile.t])
+        # self.screen.print_at("┌─────────┐", x, y)
+        # for i in range(1, 5):
+        #     self.screen.print_at("│", x, y + i)
+        # for i in range(1, 5):
+        #     self.screen.print_at("│", x + 10, y + i)
+        # self.screen.print_at("└─────────┘", x, y + 5)
+        # self.screen.print_at(f"{tile.h}", x + 5, y + 1, colour=169)
+        # self.screen.print_at(f"{tile.f}", x + 2, y + 2, colour=28)
+        # self.screen.print_at(f"{tile.i}", x + 8, y + 2, colour=136)
+        # self.screen.print_at(f"{tile.r}", x + 5, y + 3)
+        # self.screen.print_at(f"{tile.g}", x + 2, y + 4, colour=226)
+        # self.screen.print_at(f"{tile.s}", x + 8, y + 4, colour=21)
 
 
 Screen.wrapper(Game)
--- map.py
+++ map.py
@@ -5,6 +5,14 @@ from typing import List
 
 resources = {'diamond': '💎'}
 
+terrain = {
+    'desert': (0, 0.1, 2.5, 1.0, -0.2),
+    'forest': (3, 0, 2, 1, 1),
+    'ocean': (1, 0, 0, 0, 0),
+    'plains': (1, 1, 1, 1, 1),
+    'mountain': (0.5, 2, 1, 3, 0.5)
+}
+
 @dataclass
 class Tile:
     f: int # food
@@ -13,9 +21,10 @@ class Tile:
     s: int # science
     h: int # happiness
     r: str # resource
+    t: str # terrain
 
     @classmethod
-    def generate(cls):
+    def generate(cls, t):
         f, g, i, s, h = [int(random.random() * random.randint(1, 6)) for _ in range(5)]
 
         if random.random() > 0.99:
@@ -23,12 +32,15 @@ class Tile:
         else:
             r = ' '
 
+        a, b, c, d, e = terrain[t]
 
-        return cls(f, g, i, s, h, r)
+
+        return cls(f * a, g * b, i * c, s * d, h * e, r, t)
 
 
 class Map:
-    def __init__(self, size):
+    def __init__(self):
+        size = 5
         self.map = [[Tile.generate() for _ in range(size)] for _ in range(size)]
 
     def at(self, x, y):
